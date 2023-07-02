cd604eda64cf30993badcad1fc9a89eaa2860dd8
vanbelt <makaov.maks.v@yandex.ru>
2023-07-02 19:45:41 +0300

    Initial commit

---
+++ main/java/com/example/springtransportsdrivers/SpringTransportsDriversApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtransportsdrivers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransportsDriversApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTransportsDriversApplication.class, args);
+    }
+
+}
---
+++ test/java/com/example/springtransportsdrivers/SpringTransportsDriversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtransportsdrivers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransportsDriversApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
